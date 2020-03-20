6f5cb0daa887d396e9f96170fdefcae6502d65be
Mura <35004798+mura-orz@users.noreply.github.com>
2020-03-20 15:15:19 +0900

    Update xxx.hxx
    adds documentation

--- xxx/xxx.hxx
+++ xxx/xxx.hxx
@@ -22,9 +22,27 @@ namespace xxx {
 ///	@return		version of built version
 std::uint32_t	get_version() noexcept;
 
+///	@brief	Gets major version of the @p version.
+///		If backward compatibility is broken, this version is imcremented.
+///	@param[in]	version		The library versions, xxx_version macro or result of xxx_version()
+///	@return		Major version of the @p version.
 inline std::uint32_t	get_major_version(std::uint32_t version) noexcept    { return static_cast<std::uint32_t>((version >> 24) & 0x000000FF);  }
+///	@brief	Gets minor version of the @p version.
+///		If new feature with backward compatibility is added, this version is imcremented.
+///		If the major version increases, this version is reset to zero.
+///	@param[in]	version		The library versions, xxx_version macro or result of xxx_version()
+///	@return		Minor version of the @p version.
 inline std::uint32_t	get_minor_version(std::uint32_t version) noexcept    { return static_cast<std::uint32_t>((version >> 16) & 0x000000FF);  }
+///	@brief	Gets revision of the @p version.
+///		If bug-fixes or trival improvements are added, this version is imcremented.
+///		If the minor version increases or resets, this version is reset to zero, too.
+///	@param[in]	version		The library versions, xxx_version macro or result of xxx_version()
+///	@return		Revision of the @p version.
 inline std::uint32_t	get_revision(std::uint32_t version) noexcept         { return static_cast<std::uint32_t>((version >>  8) & 0x000000FF);  }
+///	@brief	Gets extra version of the @p version.
+///		This version is reserved for especial usage.
+///	@param[in]	version		The library versions, xxx_version macro or result of xxx_version()
+///	@return		Extra version of the @p version.
 inline std::uint32_t	get_extra_version(std::uint32_t version) noexcept    { return static_cast<std::uint32_t>((version >>  0) & 0x000000FF);  }
   
 }	// namespace xxx
